93fa9cb76e1e85db8215edace0da291eb6191fcd
yuki819 <yuki819@mba2018.local>
2022-07-19 00:50:19 +0900

    :rotating_light: (refactor): lintで怒られた箇所の修正

--- app/main.go
+++ app/main.go
@@ -2,21 +2,9 @@ package main
 
 import (
 	"fmt"
-	"math"
 
 	"rsc.io/quote"
 )
-
-var unused int // ERROR "`unused` is unused"
-
-func nouse() {
-	x := math.MinInt8
-	for {
-		_ = x
-		x = 0 // ERROR "ineffectual assignment to x"
-		x = 0
-	}
-}
 
 func main() {
 	fmt.Println(quote.Hello())
